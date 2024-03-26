208cbea675849d9a3e7c8264eca771f666ba1606
ngcsontrh <trhsonngoc12@gmail.com>
2024-03-26 10:05:48 +0700

    update

--- app/Http/Controllers/UserInformationController.php
+++ app/Http/Controllers/UserInformationController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserInformationRequest;
-use App\Models\UserInformation;
 use Illuminate\Http\Request;
+use App\Models\UserInformation;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\UserInformationRequest;
 
 class UserInformationController extends Controller
 {
     public function edit() {
         $user = Auth::user()->userInformation;
+        Gate::authorize('view', $user);
         return view('profile', [
             'userInformation' => $user,
         ]);
@@ -18,6 +20,7 @@ class UserInformationController extends Controller
 
     public function update(UserInformationRequest $request, UserInformation $userInformation) {
         // dd($request['gender']);
+        Gate::authorize('update', $userInformation);
         $validated = $request->validated();
         $userInformation->update($validated);
         return redirect()->back();
